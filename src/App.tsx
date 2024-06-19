99c3ed6e48d7d589ac1555f160aeb417e6e059c6
r45mu5m05c4 <rasmusmosca@gmail.com>
2024-06-19 12:00:37 +0200

    changes to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Home from "./home/Home";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   return (
