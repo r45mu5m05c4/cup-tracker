4f792a2c6a92054baf59184cd4200bb83e52ac4a
r45mu5m05c4 <rasmusmosca@gmail.com>
2024-06-27 13:15:08 +0200

    TeamTable widget update

--- src/components/TeamTable/TeamTable.tsx
+++ src/components/TeamTable/TeamTable.tsx
@@ -4,6 +4,11 @@ import { getTeams } from "../../utils/queries";
 import { FC, useEffect, useState } from "react";
 import { Team } from "../../utils/types/Team";
 import { styled } from "styled-components";
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ArrowLongRightIcon,
+} from "@heroicons/react/20/solid";
 
 interface Props {
   small: boolean;
@@ -12,6 +17,7 @@ interface Props {
 const TeamTable: FC<Props> = ({ small }) => {
   const [teamsA, setTeamsA] = useState<Team[]>([]);
   const [teamsB, setTeamsB] = useState<Team[]>([]);
+  const [activeGroup, setActiveGroup] = useState<string>("A");
   const { user } = useUser();
 
   useEffect(() => {
@@ -50,26 +56,89 @@ const TeamTable: FC<Props> = ({ small }) => {
         { key: "goalsAgainst", header: "GA" },
         { key: "gamesPlayed", header: "GP" },
       ];
+
+  const toggleGroup = () => {
+    activeGroup === "A" ? setActiveGroup("B") : setActiveGroup("A");
+  };
   return (
-    <Container>
-      <Header>Group A</Header>
-      <Table data={teamsA} columns={teamColumns}></Table>
-      <Header>Group B</Header>
-      <Table data={teamsB} columns={teamColumns}></Table>
+    <Container $small={small}>
+      {small ? (
+        <>
+          <SmallHeader>
+            {activeGroup === "A" ? "Group A" : "Group B"}
+            <LeftIconButton onClick={() => toggleGroup()} />{" "}
+            <RightIconButton onClick={() => toggleGroup()} />
+          </SmallHeader>
+          <Table
+            data={activeGroup === "A" ? teamsA : teamsB}
+            columns={teamColumns}
+          ></Table>
+          <Link href="/#/teams">
+            Go to groups
+            <LinkIconButton />
+          </Link>
+        </>
+      ) : (
+        <>
+          <Header>Group A</Header>
+          <Table data={teamsA} columns={teamColumns}></Table>
+          <Header>Group B</Header>
+          <Table data={teamsB} columns={teamColumns}></Table>
+        </>
+      )}
     </Container>
   );
 };
 
 export default TeamTable;
 
-const Container = styled.div`
+const Container = styled.div<{ $small: boolean }>`
   @media (max-width: 768px) {
     padding: 0;
   }
-  padding: 24px;
+  padding: ${(props) => (props.$small ? "0" : "24px")};
 `;
 const Header = styled.h2`
   @media (max-width: 768px) {
     padding: 10px;
+  }
+`;
+const RightIconButton = styled(ChevronRightIcon)`
+  height: 20px;
+  cursor: pointer;
+  &:hover {
+    color: #42917e;
+  }
+`;
+
+const LeftIconButton = styled(ChevronLeftIcon)`
+  height: 20px;
+  margin-left: 10px;
+  cursor: pointer;
+  &:hover {
+    color: #42917e;
+  }
+`;
+const LinkIconButton = styled(ArrowLongRightIcon)`
+  height: 20px;
+  cursor: pointer;
+  margin: auto;
+  margin-left: 5px;
+`;
+const SmallHeader = styled.div`
+  height: 10%;
+  display: flex;
+  align-items: center;
+  border-bottom: 0.5px solid;
+  padding: 10px;
+  font-weight: 500;
+`;
+const Link = styled.a`
+  display: flex;
+  align-items: center;
+  color: #000;
+  padding: 10px;
+  &:hover {
+    color: #42917e;
   }
 `;
